3a0c204f4d15dffb122c9e503e72b2504352cdb0
Bartosz Drozd <b.drozd@kainos.com>
2020-11-04 09:23:13 +0000

    bugfix/RTA-91 Displaying error for unexisting postcode

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 import { logger } from '../utils/logger.util';
-import { postcodeUtils, FormError } from '../utils/postcode.util';
+import { postcodeUtils } from '../utils/postcode.util';
 import { geolocationService } from '../services/geolocation.service';
 import { AuthorisedTestingFacility } from '../models/authorisedTestingFacility.model';
 import { PagedResponse } from '../models/pagedResponse.model';
 import { pagination } from '../utils/pagination.util';
 import { PaginationOptions } from '../models/paginationOptions.model';
+import { getDefaultPostcodeFormError } from '../errors/postcode.error';
 
 const PAGINATION_ITEMS_PER_PAGE = 5;
 const PAGINATION_MAX_NUMBER_OF_PAGES = 10;
@@ -19,22 +20,25 @@ export const search = async (req: Request, res: Response, next: NextFunction): P
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const postcode: string = (<string> req.query.postcode || '').toUpperCase().replace(/\s+/g, '');
     logger.info(req, `Search for postcode ${postcode}`);
-    const errors: Array<FormError> = postcodeUtils.validate(postcode);
 
-    if (errors.length !== 0) {
+    if (!postcodeUtils.isValidPostcode(postcode)) {
       return res.render('search/show', {
         hasError: true,
-        formErrors: errors[0],
+        formErrors: getDefaultPostcodeFormError(),
       });
     }
 
     const currentPage = pagination.getCurrentPageFromRequest(req, PAGINATION_MAX_NUMBER_OF_PAGES);
     const paginationOptions: PaginationOptions = { page: currentPage, limit: PAGINATION_ITEMS_PER_PAGE };
-
     // eslint-disable-next-line max-len
-    const pagedResponse: PagedResponse<AuthorisedTestingFacility> = pagination.isInMaxNumberOfPagesRange(currentPage, PAGINATION_MAX_NUMBER_OF_PAGES)
-      ? (await geolocationService.nearest(req, postcode, paginationOptions))
-      : <PagedResponse<AuthorisedTestingFacility>> {};
+    const pagedResponse: PagedResponse<AuthorisedTestingFacility> = await geolocationService.nearest(req, postcode, paginationOptions);
+
+    if (pagedResponse.Items === undefined) {
+      return res.render('search/show', {
+        hasError: true,
+        formErrors: getDefaultPostcodeFormError(),
+      });
+    }
 
     return res.render('search/results', {
       search: postcode,
--- src/utils/postcode.util.ts
+++ src/utils/postcode.util.ts
@@ -1,32 +1,14 @@
 import { isValid, toNormalised } from 'postcode';
 
-interface FieldError {
-  field: string,
-  message: string,
-}
-export interface FormError {
-  heading: string,
-  errors: FieldError[],
-  errorMessage: string,
-}
+const isValidPostcode = (data: string): boolean => {
+  if (data.length === 0) {
+    return false;
+  }
 
-const isValidPostcode = (data: string): boolean => (data.length === 0 ? false : isValid(data));
-
-const validate = (data: string): Array<FormError> => {
-  const postcodeError: FormError = {
-    heading: 'There is a problem',
-    errors: [
-      {
-        field: 'postcode',
-        message: 'Enter a postcode, like SW1A 2AA',
-      },
-    ],
-    errorMessage: 'Enter a postcode in the correct format',
-  };
-  return isValidPostcode(data.toString().trim()) ? [] : [postcodeError];
+  return isValid(data.toString().trim());
 };
 
 export const postcodeUtils = {
-  validate,
+  isValidPostcode,
   toNormalised,
 };
